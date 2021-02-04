1281eed781878563e41f3d3a280c898ec3d00f29
osidekyle <hawkinskyle67@gmail.com>
2021-02-03 17:25:31 -0800

    jquery animationa and fetch api

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
     
 
-    $("#button").on("click",function(){
-        $(".quote-container").slideToggle()
+    $("#button").on("click",async function(){
+        
+        
+
+
+
+         $(".words").slideUp()
+        $(".quote-container").animate({height:"0.5px",paddingTop:"0",borderRadius:"10px"})
+        
+
+
+        var response= await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/")
+        var parsedResponse=await response.json()
+        
+
+
+
+        $(".quote-container").delay(200).animate({height:"100%",paddingTop:"30px"})
+        $(".words").slideDown(1000) 
     })
 
     
