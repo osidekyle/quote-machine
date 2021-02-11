e1c34ce70c02dc6e41b41e3d9a7a4971f0154585
osidekyle <hawkinskyle67@gmail.com>
2021-02-10 17:12:08 -0800

    inserted api response in html

--- app.js
+++ app.js
@@ -11,9 +11,27 @@ $(document).ready(function(){
         $(".quote-container").animate({height:"0.5px",paddingTop:"0",borderRadius:"10px"})
         
 
+        
+        
 
-        var response= await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/")
+
+        var response= await fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random");
+       
         var parsedResponse=await response.json()
+
+        console.log(parsedResponse.data[0].quoteText)
+
+        $("#quote-text").text(parsedResponse.data[0].quoteText)
+        $("#quote-name").text("- "+parsedResponse.data[0].quoteAuthor)
+        
+        /* var lengthOfList=parsedResponse.data.length
+
+        
+
+        var randomNumber=Math.floor(Math.random()*lengthOfList)
+        console.log(randomNumber)
+
+        console.log(parsedResponse.data[randomNumber]) */
         
 
 
